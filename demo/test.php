48981f41c3cfccb5d28d18353083146ba03c6ab4
DoctorAlien <shenjiajun2009@126.com>
2015-12-14 11:56:24 +0800

    新增字体路径
    新增字体路径

--- demo/test.php
+++ demo/test.php
@@ -1,6 +1,6 @@
 <?php
-	require"portrait.class.php";
-	$image=new Portrait();
+	require"portrait.class.php";                    //引用文件
+	$image=new Portrait();                          //实例化
 	$image->name_string="这是一个测试!Thisistest";  //昵称
 	$image->name_selected=rand(0,16);				//第几位昵称
 	$image->img_width=rand(100,500);				//图片宽度
@@ -10,5 +10,6 @@
 	$image->fontcolor_red=rand(0,255);				//字体颜色rgb
 	$image->fontcolor_green=rand(0,255);
 	$image->fontcolor_blue=rand(0,255);
+	$image->font_file='./font/simhei.ttf' ;			//字体文件
 	$image->imgShow();								//显示图片
 ?>
